602619b9d5ca199f366965360178cb78db772aae
Evandro <evandromoura@gmail.com>
2021-05-15 14:55:32 -0300

    Desenv

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-if [ $(docker ps -a -f name=jkubecluster | grep -w jkubecluster | wc -l) -eq 1 ]; then
-  docker rm -f jkubecluster
-fi
-mvn clean package && docker build -t evandromoura/jkubecluster .
-docker run -d -p 9080:9080 -p 9443:9443 --name jkubecluster evandromoura/jkubecluster
+mvn clean package && sudo docker build -t evandromoura/jkubecluster . && sudo docker push evandromoura/jkubecluster
